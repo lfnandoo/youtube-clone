48454dfd129c72e712b096c866f9da4740e1b1a5
Luiz Fernando <devfernandoof@gmail.com>
2020-08-30 23:56:22 -0300

    🚧 header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { ThemeProvider, createMuiTheme, makeStyles } from "@material-ui/core";
+import Home from "./Home";
+
+const useStyles = makeStyles({
+  root: {},
+});
 
 function App() {
+  const theme = createMuiTheme({
+    pallete: {
+      primary: {
+        main: "#f44336",
+      },
+      secondary: {
+        main: "#3f51b5",
+      },
+    },
+  });
+
+  const classes = useStyles();
+
   return (
-    
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,74 @@
+import React from "react";
+import {
+  makeStyles,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+
+import VideoCall from "@material-ui/icons/VideoCall";
+import MoreVert from "@material-ui/icons/MoreVert";
+import Apps from "@material-ui/icons/Apps";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  appBar: {
+    boxShadow: "none",
+  },
+  menuIcon: {
+    paddingRight: theme.spacing(5),
+    paddingLeft: theme.spacing(6),
+  },
+  icons: {
+    paddingRight: theme.spacing(5),
+  },
+  grow: {
+    flexGrow: 1,
+  },
+}));
+
+function Home() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar color="inherit" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuIcon}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <div className={classes.grow} />
+          <IconButton className={classes.icons} color="inherit">
+            <VideoCall />
+          </IconButton>
+          <IconButton className={classes.icons} color="inherit">
+            <Apps />
+          </IconButton>
+          <IconButton className={classes.icons} color="inherit">
+            <MoreVert />
+          </IconButton>
+          <Button
+            startIcon={<AccountCircle />}
+            variant="outlined"
+            color="primary"
+          >
+            Fazer Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Home;
